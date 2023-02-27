318493e6ca925975820c86ade0a4c783c377890a
santosh_balaji <selvarajs@artc.a-star.edu.sg>
2023-02-27 19:01:15 +0800

    feat: added control system logic

--- src/arduino_actuator_interface.cpp
+++ src/arduino_actuator_interface.cpp
@@ -3,46 +3,47 @@
 CallbackReturn ArduinoActuatorInterface::on_init(
   const hardware_interface::HardwareInfo & info)
 {
-
+  return CallbackReturn::SUCCESS;
 }
 
 CallbackReturn ArduinoActuatorInterface::on_configure(
   const rclcpp_lifecycle::State & previous_state)
 {
-
+  return CallbackReturn::SUCCESS;
 }
-
 
 std::vector<hardware_interface::StateInterface> ArduinoActuatorInterface::export_state_interfaces()
 {
-
+  std::vector<hardware_interface::StateInterface> state_interfaces;
+  return state_interfaces;
 }
 
 std::vector<hardware_interface::CommandInterface> ArduinoActuatorInterface::export_command_interfaces()
 {
-
+  std::vector<hardware_interface::CommandInterface> command_interafaces;
+  return command_interafaces;
 }
 
 CallbackReturn ArduinoActuatorInterface::on_activate(
   const rclcpp_lifecycle::State & previous_state)
 {
-
+  return CallbackReturn::SUCCESS;
 }
 
 CallbackReturn ArduinoActuatorInterface::on_deactivate(
   const rclcpp_lifecycle::State & previous_state)
 {
-
+  return CallbackReturn::SUCCESS;
 }
 
 hardware_interface::return_type ArduinoActuatorInterface::read()
 {
-
+  return hardware_interface::return_type::OK;
 }
 
 hardware_interface::return_type ArduinoActuatorInterface::write()
 {
-
+  return hardware_interface::return_type::OK;
 }
 
 #include "pluginlib/class_list_macros.hpp"
