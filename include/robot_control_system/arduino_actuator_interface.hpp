0b9cd3f6c4b58b1472dd07592e27dedca1ecc3e1
santoshbalaji <santechselva@gmail.com>
2023-03-05 02:34:06 +0800

    feat : updated with position only codes

--- include/robot_control_system/arduino_actuator_interface.hpp
+++ include/robot_control_system/arduino_actuator_interface.hpp
@@ -9,31 +9,48 @@
 #include "hardware_interface/hardware_info.hpp"
 #include "hardware_interface/system_interface.hpp"
 #include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
 
+#include "rclcpp/rclcpp.hpp"
 #include "rclcpp/macros.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
 
+#include "robot_control_system/visibility_control.h"
+
 
 using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+namespace robot_control_system
+{
 
 class ArduinoActuatorInterface : public hardware_interface::SystemInterface
 {
 public:
+  RCLCPP_SHARED_PTR_DEFINITIONS(ArduinoActuatorInterface);
+
+  ROBOT_CONTROL_SYSTEM_PUBLIC
   CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
 
+  ROBOT_CONTROL_SYSTEM_PUBLIC
   CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
 
+  ROBOT_CONTROL_SYSTEM_PUBLIC
   std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
 
+  ROBOT_CONTROL_SYSTEM_PUBLIC
   std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
 
+  ROBOT_CONTROL_SYSTEM_PUBLIC
   CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
 
+  ROBOT_CONTROL_SYSTEM_PUBLIC
   CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
 
+  ROBOT_CONTROL_SYSTEM_PUBLIC
   hardware_interface::return_type read() override;
 
+  ROBOT_CONTROL_SYSTEM_PUBLIC
   hardware_interface::return_type write() override;
 
 private:
@@ -46,5 +63,7 @@ private:
   std::vector<double> hw_commands_;
   std::vector<double> hw_states_;
 };
+
+} // namespace robot_control_system
 
 #endif  // ROBOT_CONTROL_SYSTEM__ARDUINO_ACTUATOR_INTERFACE_HPP_
--- src/arduino_actuator_interface.cpp
+++ src/arduino_actuator_interface.cpp
@@ -1,52 +1,207 @@
 #include <robot_control_system/arduino_actuator_interface.hpp>
 
+namespace robot_control_system
+{
+
 CallbackReturn ArduinoActuatorInterface::on_init(
   const hardware_interface::HardwareInfo & info)
 {
+  if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS)
+  {
+    return CallbackReturn::ERROR;
+  }
+
+  // START: This part here is for exemplary purposes - Please do not copy to your production code
+  hw_start_sec_ = stod(info_.hardware_parameters["example_param_hw_start_duration_sec"]);
+  hw_stop_sec_ = stod(info_.hardware_parameters["example_param_hw_stop_duration_sec"]);
+  hw_slowdown_ = stod(info_.hardware_parameters["example_param_hw_slowdown"]);
+  // END: This part here is for exemplary purposes - Please do not copy to your production code
+  hw_states_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  hw_commands_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+
+  for (const hardware_interface::ComponentInfo & joint : info_.joints)
+  {
+    // RRBotSystemPositionOnly has exactly one state and command interface on each joint
+    if (joint.command_interfaces.size() != 1)
+    {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("RRBotSystemPositionOnlyHardware"),
+        "Joint '%s' has %zu command interfaces found. 1 expected.", joint.name.c_str(),
+        joint.command_interfaces.size());
+      return CallbackReturn::ERROR;
+    }
+
+    if (joint.command_interfaces[0].name != hardware_interface::HW_IF_POSITION)
+    {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("RRBotSystemPositionOnlyHardware"),
+        "Joint '%s' have %s command interfaces found. '%s' expected.", joint.name.c_str(),
+        joint.command_interfaces[0].name.c_str(), hardware_interface::HW_IF_POSITION);
+      return CallbackReturn::ERROR;
+    }
+
+    if (joint.state_interfaces.size() != 1)
+    {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("RRBotSystemPositionOnlyHardware"),
+        "Joint '%s' has %zu state interface. 1 expected.", joint.name.c_str(),
+        joint.state_interfaces.size());
+      return CallbackReturn::ERROR;
+    }
+
+    if (joint.state_interfaces[0].name != hardware_interface::HW_IF_POSITION)
+    {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("RRBotSystemPositionOnlyHardware"),
+        "Joint '%s' have %s state interface. '%s' expected.", joint.name.c_str(),
+        joint.state_interfaces[0].name.c_str(), hardware_interface::HW_IF_POSITION);
+      return CallbackReturn::ERROR;
+    }
+  }
+
   return CallbackReturn::SUCCESS;
 }
 
 CallbackReturn ArduinoActuatorInterface::on_configure(
   const rclcpp_lifecycle::State & previous_state)
 {
+  // START: This part here is for exemplary purposes - Please do not copy to your production code
+  RCLCPP_INFO(
+    rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Configuring ...please wait...");
+
+  for (int i = 0; i < hw_start_sec_; i++)
+  {
+    rclcpp::sleep_for(std::chrono::seconds(1));
+    RCLCPP_INFO(
+      rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "%.1f seconds left...",
+      hw_start_sec_ - i);
+  }
+  // END: This part here is for exemplary purposes - Please do not copy to your production code
+
+  // reset values always when configuring hardware
+  for (uint i = 0; i < hw_states_.size(); i++)
+  {
+    hw_states_[i] = 0;
+    hw_commands_[i] = 0;
+  }
+
+  RCLCPP_INFO(rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Successfully configured!");
+
   return CallbackReturn::SUCCESS;
 }
 
 std::vector<hardware_interface::StateInterface> ArduinoActuatorInterface::export_state_interfaces()
 {
   std::vector<hardware_interface::StateInterface> state_interfaces;
+  for (uint i = 0; i < info_.joints.size(); i++)
+  {
+    state_interfaces.emplace_back(hardware_interface::StateInterface(
+      info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_states_[i]));
+  }
   return state_interfaces;
 }
 
 std::vector<hardware_interface::CommandInterface> ArduinoActuatorInterface::export_command_interfaces()
 {
-  std::vector<hardware_interface::CommandInterface> command_interafaces;
-  return command_interafaces;
+  std::vector<hardware_interface::CommandInterface> command_interfaces;
+  for (uint i = 0; i < info_.joints.size(); i++)
+  {
+    command_interfaces.emplace_back(hardware_interface::CommandInterface(
+      info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_commands_[i]));
+  }
+
+  return command_interfaces;
 }
 
 CallbackReturn ArduinoActuatorInterface::on_activate(
   const rclcpp_lifecycle::State & previous_state)
 {
+  // START: This part here is for exemplary purposes - Please do not copy to your production code
+  RCLCPP_INFO(
+    rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Activating ...please wait...");
+
+  for (int i = 0; i < hw_start_sec_; i++)
+  {
+    rclcpp::sleep_for(std::chrono::seconds(1));
+    RCLCPP_INFO(
+      rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "%.1f seconds left...",
+      hw_start_sec_ - i);
+  }
+  // END: This part here is for exemplary purposes - Please do not copy to your production code
+
+  // command and state should be equal when starting
+  for (uint i = 0; i < hw_states_.size(); i++)
+  {
+    hw_commands_[i] = hw_states_[i];
+  }
+
+  RCLCPP_INFO(rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Successfully activated!");
+
   return CallbackReturn::SUCCESS;
 }
 
 CallbackReturn ArduinoActuatorInterface::on_deactivate(
   const rclcpp_lifecycle::State & previous_state)
 {
+  // START: This part here is for exemplary purposes - Please do not copy to your production code
+  RCLCPP_INFO(
+    rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Deactivating ...please wait...");
+
+  for (int i = 0; i < hw_stop_sec_; i++)
+  {
+    rclcpp::sleep_for(std::chrono::seconds(1));
+    RCLCPP_INFO(
+      rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "%.1f seconds left...",
+      hw_stop_sec_ - i);
+  }
+
+  RCLCPP_INFO(rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Successfully deactivated!");
+  // END: This part here is for exemplary purposes - Please do not copy to your production code
+
   return CallbackReturn::SUCCESS;
 }
 
 hardware_interface::return_type ArduinoActuatorInterface::read()
 {
+  // START: This part here is for exemplary purposes - Please do not copy to your production code
+  RCLCPP_INFO(rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Reading...");
+
+  for (uint i = 0; i < hw_states_.size(); i++)
+  {
+    // Simulate RRBot's movement
+    hw_states_[i] = hw_states_[i] + (hw_commands_[i] - hw_states_[i]) / hw_slowdown_;
+    RCLCPP_INFO(
+      rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Got state %.5f for joint %d!",
+      hw_states_[i], i);
+  }
+  RCLCPP_INFO(rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Joints successfully read!");
+  // END: This part here is for exemplary purposes - Please do not copy to your production code
+
   return hardware_interface::return_type::OK;
 }
 
 hardware_interface::return_type ArduinoActuatorInterface::write()
 {
+  // START: This part here is for exemplary purposes - Please do not copy to your production code
+  RCLCPP_INFO(rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Writing...");
+
+  for (uint i = 0; i < hw_commands_.size(); i++)
+  {
+    // Simulate sending commands to the hardware
+    RCLCPP_INFO(
+      rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Got command %.5f for joint %d!",
+      hw_commands_[i], i);
+  }
+  RCLCPP_INFO(
+    rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Joints successfully written!");
+  // END: This part here is for exemplary purposes - Please do not copy to your production code
+
   return hardware_interface::return_type::OK;
 }
+
+} // namespace robot_control_system
 
 #include "pluginlib/class_list_macros.hpp"
 
 PLUGINLIB_EXPORT_CLASS(
-  ArduinoActuatorInterface, hardware_interface::SystemInterface)
+  robot_control_system::ArduinoActuatorInterface, hardware_interface::SystemInterface)
